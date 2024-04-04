72a8880d4e6fa9694481464fb7029a0aab354bf3
Mykola <mykola@gmail.com>
2024-04-04 12:35:38 +0300

    added before method

--- src/test/java/tests/EmptySidebarCartTest.java
+++ src/test/java/tests/EmptySidebarCartTest.java
@@ -2,14 +2,22 @@ package tests;
 
 import fragments.HeaderFragment;
 import fragments.SidebarCartFragment;
-import io.qameta.allure.Description;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class EmptySidebarCartTest extends BaseTest {
 
-    @Test(description = "Empty Sidebar Cart Verification")
-    @Description("SPRIN-12")
+    public static HeaderFragment headerFragment;
+    public static SidebarCartFragment sidebarCartFragment;
+
+    @BeforeMethod
+    public void beforeTest() {
+        headerFragment = new HeaderFragment(driver);
+        sidebarCartFragment = new SidebarCartFragment(driver);
+    }
+
+    @Test(description = "SPRIN-12 Empty Sidebar Cart Verification")
 
     public void emptySidebarCart() {
         HeaderFragment headerFragment = new HeaderFragment(driver);
