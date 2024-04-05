217f81716407989596a5a1f5841904ea2887d6b1
yungboigrek <[illiagrek@ukr.net]>
2024-04-05 13:44:06 +0300

    save6

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -9,8 +9,19 @@ public class Homepage extends HeaderFragment {
     public Homepage(WebDriver driver) {
         super(driver);
     }
+
     public void clickOnVansPremiumButton() {
         WebElement element = driver.findElement(By.xpath("//a[@href=/shop/en-gb/vans-gb/premium]"));
+        element.click();
+    }
+
+    public void clickOnChatButton() {
+        WebElement element = driver.findElement(By.xpath("//button[contains(@class, 'helpButton')]"));
+        element.click();
+    }
+
+    public void clickOnCloseChatButton() {
+        WebElement element = driver.findElement(By.xpath("//button[contains(@class, 'helpButton')]"));
         element.click();
     }
 }
--- src/test/java/tests/OpenAndCloseChatTest.java
+++ src/test/java/tests/OpenAndCloseChatTest.java
@@ -4,8 +4,6 @@ import common.BasePage;
 import org.testng.annotations.Test;
 import pages.Homepage;
 
-import static common.BasePage.driver;
-
 @Test
 public class OpenAndCloseChatTest extends BaseTest {
 
