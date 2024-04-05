4ceb884232127e9369579540f86c4ac648461270
Santakloun1 <santakloun1@gmail.com>
2024-04-05 13:44:11 +0300

    test finished

--- src/main/java/fragments/HeaderFragment.java
+++ src/main/java/fragments/HeaderFragment.java
@@ -3,11 +3,14 @@ package fragments;
 import common.BasePage;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HeaderFragment extends BasePage {
 
     private static final String SIGN_IN = "//div[contains(@class,'cart-container')]//a[@data-open='utilityNavModal7']";
     private static final String COOKIES_ACCEPTED = "//button[@id='onetrust-accept-btn-handler']";
+    private static final String SEARCH_FIELD = "//*[@name='searchTerm']";
+    private static final String CLEAR_BUTTON_SEARCH_FIELD = "//*[@ class='topnav-clear-input']";
 
     public HeaderFragment(WebDriver driver) {
         super(driver);
@@ -22,5 +25,17 @@ public class HeaderFragment extends BasePage {
 
     public void clickSignIn() {
         waitClickabilityOfElement(SIGN_IN).click();
+    }
+
+    public WebElement getSearchField() {
+        return waitVisibilityOfElement(SEARCH_FIELD);
+    }
+
+    public WebElement getClearButtonSearchField() {
+        return waitVisibilityOfElement(CLEAR_BUTTON_SEARCH_FIELD);
+    }
+
+    public boolean isButtonDisplayed() {
+        return waitVisibilityOfElement(CLEAR_BUTTON_SEARCH_FIELD) !=null;
     }
 }
