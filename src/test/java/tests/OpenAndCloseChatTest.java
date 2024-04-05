1691be1a3577edf1bb2b2cbee386e5faf15c6e26
yungboigrek <[illiagrek@ukr.net]>
2024-04-05 13:22:33 +0300

    save2

--- src/test/java/tests/OpenAndCloseChatTest.java
+++ src/test/java/tests/OpenAndCloseChatTest.java
@@ -10,7 +10,7 @@ import static common.BasePage.driver;
 
 
 @Test
-public class OpenAndCloseChatTest{
+public class OpenAndCloseChatTest extends BaseTest{
     .clickOnVansPremiumButton();
     .clickOnChatButton();
     .clickOnCloseChatButton();
