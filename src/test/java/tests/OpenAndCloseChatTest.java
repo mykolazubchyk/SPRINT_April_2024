b0aba6f88c7a7cca1e80cab45c991868ffe14a35
yungboigrek <[illiagrek@ukr.net]>
2024-04-05 13:21:20 +0300

    save1

---
+++ src/test/java/tests/OpenAndCloseChatTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import common.BasePage;
+import org.testng.annotations.Test;
+import pages.Homepage;
+
+import static common.BasePage.driver;
+
+
+
+
+@Test
+public class OpenAndCloseChatTest{
+    .clickOnVansPremiumButton();
+    .clickOnChatButton();
+    .clickOnCloseChatButton();
+
+}
